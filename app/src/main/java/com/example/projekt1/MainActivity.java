cb03d1777564f1cf2e1356b992d1149216458c07
duer <daniel.marzol@yahoo.com>
2021-03-25 15:46:15 +0100

    Pierwsze poprawki v2

--- app/src/main/java/com/example/projekt1/MainActivity.java
+++ app/src/main/java/com/example/projekt1/MainActivity.java
@@ -2,13 +2,8 @@ package com.example.projekt1;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
     }
 
